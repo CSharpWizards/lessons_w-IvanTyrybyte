8e28e9968899e7039d716aa6c0e0a8341757d22c
Unium <36132936+Pasha123456754212@users.noreply.github.com>
2018-03-29 20:34:06 +0300

    06 Зан.

---
+++ 06/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты x");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координаты y");
+            int y = int.Parse(Console.ReadLine());
+            if (x > 4)
+            {
+                Console.WriteLine("Точка попала в 2 область");
+            }
+            else
+            {
+                Console.WriteLine("Точка попала в 1 область");
+            }
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
---
+++ 06/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rost");
+            int rost = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ves");
+            int ves = int.Parse(Console.ReadLine());
+            if (ves > rost - 100)
+            {
+                Console.WriteLine("Вам нужно похудеть");
+            }
+            else
+            {
+                Console.WriteLine("Вам нужно поправиться");
+            }
+            Console.ReadLine();
+        }
+    }
+}
